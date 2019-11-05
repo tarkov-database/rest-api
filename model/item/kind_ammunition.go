bfd6986f4cfa8474b2fa6d590851157059345f88
Markus Wiegand <mail@morphy2k.dev>
2019-11-05 22:04:38 +0100

    add ammunition weapon modifier

--- model/item/kind_ammunition.go
+++ model/item/kind_ammunition.go
@@ -9,18 +9,19 @@ const (
 type Ammunition struct {
 	Item `bson:",inline"`
 
-	Caliber             string   `json:"caliber" bson:"caliber"`
-	Type                string   `json:"type" bson:"type"`
-	Tracer              bool     `json:"tracer" bson:"tracer"`
-	TracerColor         string   `json:"tracerColor" bson:"tracerColor"`
-	Subsonic            bool     `json:"subsonic" bson:"subsonic"`
-	Velocity            float64  `json:"velocity" bson:"velocity"`
-	BallisticCoeficient float64  `json:"ballisticCoef" bson:"ballisticCoef"`
-	Damage              float64  `json:"damage" bson:"damage"`
-	Penetration         float64  `json:"penetration" bson:"penetration"`
-	ArmorDamage         float64  `json:"armorDamage" bson:"armorDamage"`
-	Fragmentation       AmmoFrag `json:"fragmentation" bson:"fragmentation"`
-	Pellets             int64    `json:"pellets,omitempty" bson:"pellets,omitempty"`
+	Caliber             string         `json:"caliber" bson:"caliber"`
+	Type                string         `json:"type" bson:"type"`
+	Tracer              bool           `json:"tracer" bson:"tracer"`
+	TracerColor         string         `json:"tracerColor" bson:"tracerColor"`
+	Subsonic            bool           `json:"subsonic" bson:"subsonic"`
+	Velocity            float64        `json:"velocity" bson:"velocity"`
+	BallisticCoeficient float64        `json:"ballisticCoef" bson:"ballisticCoef"`
+	Damage              float64        `json:"damage" bson:"damage"`
+	Penetration         float64        `json:"penetration" bson:"penetration"`
+	ArmorDamage         float64        `json:"armorDamage" bson:"armorDamage"`
+	Fragmentation       AmmoFrag       `json:"fragmentation" bson:"fragmentation"`
+	Pellets             int64          `json:"pellets,omitempty" bson:"pellets,omitempty"`
+	WeaponModifier      WeaponModifier `json:"weaponModifier" bson:"weaponModifier"`
 }
 
 // AmmoFrag represents the fragmentation data of Ammunition
@@ -28,4 +29,10 @@ type AmmoFrag struct {
 	Chance float64 `json:"chance" bson:"chance"`
 	Min    int64   `json:"min" bson:"min"`
 	Max    int64   `json:"max" bson:"max"`
+}
+
+// WeaponModifier contains the weapon modifiers of Ammunition
+type WeaponModifier struct {
+	Accuracy float64 `json:"accuracy" bson:"accuracy"`
+	Recoil   float64 `json:"recoil" bson:"recoil"`
 }
