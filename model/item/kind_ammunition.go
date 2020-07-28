8bbf4ed8a2484a6e50944a36ea2fe6025e75ae7d
Markus Wiegand <mail@morphy2k.dev>
2020-07-28 16:59:43 +0200

    add ammo grenade properties

--- model/item/kind_ammunition.go
+++ model/item/kind_ammunition.go
@@ -9,20 +9,21 @@ const (
 type Ammunition struct {
 	Item `bson:",inline"`
 
-	Caliber             string         `json:"caliber" bson:"caliber"`
-	Type                string         `json:"type" bson:"type"`
-	Tracer              bool           `json:"tracer" bson:"tracer"`
-	TracerColor         string         `json:"tracerColor" bson:"tracerColor"`
-	Subsonic            bool           `json:"subsonic" bson:"subsonic"`
-	Velocity            float64        `json:"velocity" bson:"velocity"`
-	BallisticCoeficient float64        `json:"ballisticCoef" bson:"ballisticCoef"`
-	Damage              float64        `json:"damage" bson:"damage"`
-	Penetration         float64        `json:"penetration" bson:"penetration"`
-	ArmorDamage         float64        `json:"armorDamage" bson:"armorDamage"`
-	Fragmentation       AmmoFrag       `json:"fragmentation" bson:"fragmentation"`
-	Projectiles         int64          `json:"projectiles" bson:"projectiles"`
-	Pellets             int64          `json:"pellets,omitempty" bson:"pellets,omitempty"` // deprecated
-	WeaponModifier      WeaponModifier `json:"weaponModifier" bson:"weaponModifier"`
+	Caliber             string                 `json:"caliber" bson:"caliber"`
+	Type                string                 `json:"type" bson:"type"`
+	Tracer              bool                   `json:"tracer" bson:"tracer"`
+	TracerColor         string                 `json:"tracerColor" bson:"tracerColor"`
+	Subsonic            bool                   `json:"subsonic" bson:"subsonic"`
+	Velocity            float64                `json:"velocity" bson:"velocity"`
+	BallisticCoeficient float64                `json:"ballisticCoef" bson:"ballisticCoef"`
+	Damage              float64                `json:"damage" bson:"damage"`
+	Penetration         float64                `json:"penetration" bson:"penetration"`
+	ArmorDamage         float64                `json:"armorDamage" bson:"armorDamage"`
+	Fragmentation       AmmoFrag               `json:"fragmentation" bson:"fragmentation"`
+	Projectiles         int64                  `json:"projectiles" bson:"projectiles"`
+	Pellets             int64                  `json:"pellets,omitempty" bson:"pellets,omitempty"` // deprecated
+	WeaponModifier      WeaponModifier         `json:"weaponModifier" bson:"weaponModifier"`
+	GrenadeProperties   *AmmoGrenadeProperties `json:"grenadeProps,omitempty" bson:"grenadeProps,omitempty"`
 }
 
 // AmmoFrag represents the fragmentation data of Ammunition
@@ -36,4 +37,12 @@ type AmmoFrag struct {
 type WeaponModifier struct {
 	Accuracy float64 `json:"accuracy" bson:"accuracy"`
 	Recoil   float64 `json:"recoil" bson:"recoil"`
+}
+
+// AmmoGrenadeProperties represents the grenade properties of Ammunition
+type AmmoGrenadeProperties struct {
+	Delay         float64 `json:"delay" bson:"delay"`
+	FragmentCount float64 `json:"fragCount" bson:"fragCount"`
+	MinRadius     float64 `json:"minRadius" bson:"minRadius"`
+	MaxRadius     float64 `json:"maxRadius" bson:"maxRadius"`
 }
