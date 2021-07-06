e4eb1484b44b7d8a3d9abd502068ca64465263f8
Markus Wiegand <mail@morphy2k.dev>
2021-07-06 23:04:09 +0200

    add operating resources

--- model/item/kind_firearm.go
+++ model/item/kind_firearm.go
@@ -9,20 +9,21 @@ const (
 type Firearm struct {
 	Item `bson:",inline"`
 
-	Type              string   `json:"type" bson:"type"`
-	Class             string   `json:"class" bson:"class"`
-	Caliber           string   `json:"caliber" bson:"caliber"`
-	RateOfFire        int64    `json:"rof" bson:"rof"`
-	BurstRounds       int64    `json:"burstRounds,omitempty" bson:"burstRounds,omitempty"`
-	Action            string   `json:"action" bson:"action"`
-	Modes             []string `json:"modes" bson:"modes"`
-	Velocity          float64  `json:"velocity" bson:"velocity"`
-	EffectiveDistance int64    `json:"effectiveDist" bson:"effectiveDist"`
-	ErgonomicsFloat   float64  `json:"ergonomicsFP" bson:"ergonomicsFP"`
-	Ergonomics        int64    `json:"ergonomics" bson:"ergonomics"` // Deprecated
-	FoldRectractable  bool     `json:"foldRectractable" bson:"foldRectractable"`
-	RecoilVertical    int64    `json:"recoilVertical" bson:"recoilVertical"`
-	RecoilHorizontal  int64    `json:"recoilHorizontal" bson:"recoilHorizontal"`
-	DurabilityRatio   float64  `json:"durabilityRatio" bson:"durabilityRatio"`
-	Slots             Slots    `json:"slots" bson:"slots"`
+	Type               string   `json:"type" bson:"type"`
+	Class              string   `json:"class" bson:"class"`
+	Caliber            string   `json:"caliber" bson:"caliber"`
+	RateOfFire         int64    `json:"rof" bson:"rof"`
+	BurstRounds        int64    `json:"burstRounds,omitempty" bson:"burstRounds,omitempty"`
+	Action             string   `json:"action" bson:"action"`
+	Modes              []string `json:"modes" bson:"modes"`
+	Velocity           float64  `json:"velocity" bson:"velocity"`
+	EffectiveDistance  int64    `json:"effectiveDist" bson:"effectiveDist"`
+	ErgonomicsFloat    float64  `json:"ergonomicsFP" bson:"ergonomicsFP"`
+	Ergonomics         int64    `json:"ergonomics" bson:"ergonomics"` // Deprecated
+	FoldRectractable   bool     `json:"foldRectractable" bson:"foldRectractable"`
+	RecoilVertical     int64    `json:"recoilVertical" bson:"recoilVertical"`
+	RecoilHorizontal   int64    `json:"recoilHorizontal" bson:"recoilHorizontal"`
+	OperatingResources int64    `json:"operatingResources" bson:"operatingResources"`
+	DurabilityRatio    float64  `json:"durabilityRatio" bson:"durabilityRatio"`
+	Slots              Slots    `json:"slots" bson:"slots"`
 }
